5437d5233e26559eac8e9672c53ff500be75e3b4
JayJay1024 <lygm1062@gmail.com>
2022-02-21 13:57:14 +0800

    Download fake csv data

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,38 @@ import React from 'react';
 import './App.css';
 import { Button } from 'antd';
 
+const rows = [
+  ["HhiUFcyvbmJJrNuveVBwEsqGfotsSEb1KRLVuB9dUn4GiuE", "kton", "18.2", "kusama"],
+  ["HhiUFcyvbmJJrNuveVBwEsqGfotsSEb1KRLVuB9dUn4GiuE", "kton", "18.2", "kusama"],
+  ["HhiUFcyvbmJJrNuveVBwEsqGfotsSEb1KRLVuB9dUn4GiuE", "kton", "18.2", "kusama"],
+  ["HhiUFcyvbmJJrNuveVBwEsqGfotsSEb1KRLVuB9dUn4GiuE", "kton", "18.2", "kusama"],
+] as string[][];
+
+const downloadData = (data: string, filename = 'transferx.csv', type = 'data:text/csv;charset=utf-8') => {
+  const file = new Blob(["\ufeff" + data], { type: type });
+  const url = URL.createObjectURL(file);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+
+  setTimeout(function() {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 0);
+};
+
 function App() {
+  const handleClick = () => {
+    downloadData(rows.map(e => e.join(",")).join("\n"));
+  }
+
   return (
-    <Button>I am antd Button</Button>
+    <>
+      <Button onClick={handleClick}>I am antd Button</Button>
+    </>
   );
 }
 
