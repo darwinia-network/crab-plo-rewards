16d2f31d03f7069f9b566929d31ebd1a63af1af6
JayJay1024 <lygm1062@gmail.com>
2022-05-07 18:43:36 +0800

    fix: crab precisions

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const STAGE_REWARDS_RATE = 0.075; // 7.5%
 export const MIN_KSM_REWARDS = new Big('0.00000001');
 export const DOT_PRECISIONS = new Big('10000000000');
 export const KSM_PRECISIONS = new Big('1000000000000');
-export const CRAB_PRECISIONS = new Big('10000000000');
+export const CRAB_PRECISIONS = new Big('1000000000');
 export const SUBSTRATE_PREFIX = 42;
 
 export const GET_CONTRIBUTORS_POWER = gql`
